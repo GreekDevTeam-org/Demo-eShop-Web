a28eb46ec948fbbe2398a2014963da00b5ffb1b2
kiriakosstefanidis <83819661+kiriakosstefanidis@users.noreply.github.com>
2021-05-31 15:43:06 +0300

    Adding Footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import './App.css';
 import {Navbar} from "./components/Navbar/Navbar"
@@ -17,18 +16,6 @@ function App() {
             </Router>
         </div>
     );
-=======
-import React from "react";
-import "./App.css";
-import { Navbar } from "./components/Navbar/Navbar";
-
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-    </div>
-  );
->>>>>>> 80b2071a839ae362259a952039a4abd1e67cb6ff
 }
 
 export default App;
