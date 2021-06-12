16034cc96ce53b03e68073f80172907b5fbced1f
kiriakosstefanidis <83819661+kiriakosstefanidis@users.noreply.github.com>
2021-06-12 13:06:27 +0300

    Adding basic form in UserProfile page

--- src/components/Pages/UserProfile.js
+++ src/components/Pages/UserProfile.js
@@ -1,14 +1,22 @@
-import React, { useState } from 'react'
-import { Card, Button, Alert } from "react-bootstrap"
+import React, { useState, useRef } from 'react'
+import { Card, Button, Alert, Form } from "react-bootstrap"
 import { useAuth } from '../../contexts/AuthContext'
-import { history, Link, useHistory } from "react-router-dom"
+import { history, useHistory } from "react-router-dom"
 
 export default function UserProfile() {
 
     const [error, setError] = useState("")
     const { currentUser, logout } = useAuth()
     const history = useHistory() 
-
+    const nameRef = useRef()
+    const snameRef = useRef()
+    const adressRef = useRef()
+    const emailRef = useRef()
+    const countryRef = useRef()
+    const cityRef = useRef()
+    const regeonRef = useRef()
+    const phoneRef = useRef()
+    
     async function Logout() {
         setError('')
 
@@ -30,9 +38,45 @@ export default function UserProfile() {
                 </Card.Body>    
             </Card>    
             <div>
-                <Button variant="link" onClick={Logout}>
+               
+            <Form >   
+                <Form.Group id="Name">
+                <Form.Label>Name</Form.Label>
+                <Form.Control type="name" ref={nameRef} required></Form.Control>
+                </Form.Group>
+                <Form.Group id="Second Name">
+                    <Form.Label>Second Name</Form.Label> 
+                    <Form.Control type="second name" ref={snameRef} required></Form.Control>            
+                </Form.Group>
+                <Form.Group id="Adress">
+                    <Form.Label>Adress</Form.Label>
+                    <Form.Control type="adress" ref={adressRef} required></Form.Control>             
+                </Form.Group>
+                <Form.Group id="Email">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="email" ref={emailRef} required></Form.Control>             
+                </Form.Group>
+                <Form.Group id="Country">
+                    <Form.Label>Country</Form.Label>
+                    <Form.Control type="country" ref={countryRef} required></Form.Control>             
+                </Form.Group>
+                <Form.Group id="City">
+                    <Form.Label>City</Form.Label>
+                    <Form.Control type="city" ref={cityRef} required></Form.Control>             
+                </Form.Group>
+                <Form.Group id="Regeon/State">
+                    <Form.Label>Regeon/State</Form.Label>
+                    <Form.Control type="regeon" ref={regeonRef} required></Form.Control>             
+                </Form.Group>
+                <Form.Group id="Telephone">
+                    <Form.Label>Telephone</Form.Label>
+                    <Form.Control type="phone" ref={phoneRef} required></Form.Control>             
+                </Form.Group>
+            </Form>
+            <Button  onClick={Logout}>
                     Log Out
-                </Button>
+            </Button>  
+               
             </div>
         </>
     )
